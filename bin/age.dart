056e0f98412a27605ff108763d9810beeb59fed7
shifas <shifasmeharnk@gmail.com>
2021-11-15 05:22:08 +0530

    age

--- bin/age.dart
+++ bin/age.dart
@@ -2,12 +2,13 @@ import 'dart:io';
 
 void main() {
   stdout.write("Enter Your Name");
-  stdin.readLineSync().toString();
+   var c=stdin.readLineSync().toString();
+
 
   stdout.write("Enter Your Age");
   var a=stdin.readLineSync().toString();
   var age=int.parse(a);
 
   var result = 100-age;
-  stdout.write("In $result Years You Have 100 Year Old");
+  stdout.write("In $result Years $c Have 100 Year Old");
 }
